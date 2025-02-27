c58e722ce35d6834ce4b5efb64ce4dc9dd2ad074
Shek Muktar <im.skm07@gmail.com>
2025-02-27 20:41:31 +0530

    vue-router: page navigation

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+// gotoProductsPage
+const gotoProductsPage = () => {
+	// processing ...
+	// route object -> {name: 'product.index'}
+	router.push({name: 'product.index'});
+}
+</script>
+<template>
+	<h1>Pages</h1>
+	<ol>
+<!-- 		<li>
+			<a href="/todo">Todo page</a>
+		</li> -->
+		<li>
+			<router-link :to="{name: 'home'}">Home Page</router-link>
+		</li>
+		<li>
+			<!-- <router-link to="/todo">Todo Page</router-link> -->
+			<router-link :to="{name: 'todo'}">Todo Page</router-link>
+		</li>
+		<li>
+			<router-link :to="{name: 'product.index'}">Products Page</router-link>
+		</li>
+		<li>
+			<!-- gotoProductsPage -->
+			<button @click="gotoProductsPage">Go to Products Page using button</button>
+		</li>
+	</ol>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 import TodoPage from "@/TodoPage.vue";
 import ProductPage from "@/ProductPage.vue";
+import HomePage from "@/pages/HomePage.vue";
 
 // const routes = [
 // 	{
@@ -17,16 +18,19 @@ import ProductPage from "@/ProductPage.vue";
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-  		{
-  			path: '/',
-  			component: null,
-  		},
+		{
+			path: '/',
+			name: 'home',
+			component: HomePage,
+		},
 		{
 			path: '/todo',
+			name: 'todo',
 			component: TodoPage,
 		},
 		{
 			path: '/products',
+			name: 'product.index',
 			component: ProductPage,
 		},
 	],
